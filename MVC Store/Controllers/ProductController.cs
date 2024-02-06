fa1984d6ece3b3833198af6c4c5ab53137ced30e
mBoichura <mBoichura@gmail.com>
2024-02-06 20:30:42 +0200

    Initial commit

---
+++ MVC Store/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Store.Models;
+using System.Diagnostics;
+
+namespace MVC_Store.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Phone[] phone = new Phone[3];
+            phone[0] = new Phone();
+            phone[0].Name = "IPhone 7";
+            phone[0].Category = "Smartphone";
+            phone[0].Price = 500;
+            phone[1] = new Phone();
+            phone[1].Name = "IPhone 8";
+            phone[1].Category = "Smartphone";
+            phone[1].Price = 600;
+            phone[2] = new Phone();
+            phone[2].Name = "IPhone 9";
+            phone[2].Category = "Smartphone";
+            phone[2].Price = 700;
+
+            return View(phone);
+        }
+    }
+}
---
+++ MVC Store/Models/Phone.cs
@@ -0,0 +1,9 @@
+namespace MVC_Store.Models
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
